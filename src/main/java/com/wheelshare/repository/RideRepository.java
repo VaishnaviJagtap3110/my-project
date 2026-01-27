29c57d56d00e4c5ef1c972347adf2f2eb15e07c6
VaishnaviJagtap3110 <148367299+VaishnaviJagtap3110@users.noreply.github.com>
2026-01-27 23:36:01 +0530

    Update RideRepository.java

--- src/main/java/com/wheelshare/repository/RideRepository.java
+++ src/main/java/com/wheelshare/repository/RideRepository.java
@@ -1,5 +1,9 @@
 package com.wheelshare.repository;
 
-public class RideRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wheelshare.entity.Ride;
+
+public interface RideRepository extends JpaRepository<Ride, Long>{
 
 }

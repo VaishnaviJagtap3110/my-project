102650a4df730de1c85a00add8e690a7a05110f8
VaishnaviJagtap3110 <148367299+VaishnaviJagtap3110@users.noreply.github.com>
2026-01-27 23:36:38 +0530

    Update UserRepository.java

--- src/main/java/com/wheelshare/repository/UserRepository.java
+++ src/main/java/com/wheelshare/repository/UserRepository.java
@@ -1,5 +1,9 @@
 package com.wheelshare.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wheelshare.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }

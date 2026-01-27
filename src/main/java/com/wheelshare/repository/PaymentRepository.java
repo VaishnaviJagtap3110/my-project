9e872d05e9478bfcb8ad6c04987222fe2499e5a0
VaishnaviJagtap3110 <148367299+VaishnaviJagtap3110@users.noreply.github.com>
2026-01-27 23:35:36 +0530

    Update PaymentRepository.java

--- src/main/java/com/wheelshare/repository/PaymentRepository.java
+++ src/main/java/com/wheelshare/repository/PaymentRepository.java
@@ -1,5 +1,9 @@
 package com.wheelshare.repository;
 
-public class PaymentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.wheelshare.entity.Payment;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long>{
 
 }
